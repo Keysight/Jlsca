19a77a5e0350b1e8541ee7b13f0c08c1f1f98bd8
Cees-Bart Breunesse <ceeesb@gmail.com>
2016-12-08 09:59:40 -0800

    better write (c)

--- ins2daredevil.jl
+++ ins2daredevil.jl
@@ -2,7 +2,6 @@
 #
 # Author: Cees-Bart Breunesse
 
-include("conditional.jl")
 include("trs.jl")
 
 using Trs
@@ -11,24 +10,22 @@ function ins2daredevil(filename)
 	trs = InspectorTrace(filename)
 	(data,samples) = trs[1]
 
-	guessesfn = @sprintf("data_%d_b_%d.bin", 8, length(trs))
-	samplesfn = @sprintf("samples_%d_b_%d.bin", length(samples), length(trs))
-	guesses = open(guessesfn, "w")
-	samples = open(samplesfn, "w")
+	guessesfn = @sprintf("data_UInt8_%dt.bin", length(trs))
+	samplesfn = @sprintf("samples_%s_%dt.bin", eltype(samples), length(trs))
 
-	for (data, sample) in trs
-		write(guesses, data[9:16])
-		write(samples, sample)
+	trs2 = SplitBinary(guessesfn, length(data), samplesfn, length(samples), eltype(samples), length(trs), true)
+
+	for i in 1:length(trs)
+		trs2[i] = trs[i]
 	end
+
 
 	@printf("Daredevil config:\n")
 	@printf("guess=%s %d %d\n", guessesfn, length(trs), length(trs[1][1]))
 	@printf("trace=%s %d %d\n", samplesfn, length(trs), length(trs[1][2]))
 
-	close(guesses)
-	close(samples)
-		# write(samples, reinterpret(UInt8, trace))
-
+	close(trs)
+	close(trs2)
 end
 
 ins2daredevil(ARGS[1])
