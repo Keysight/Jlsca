e15860bf3b3275c48a9f9fdd7fa598012062aa9e
Ilya Kizhvatov <i.kizhvatov@cs.ru.nl>
2017-04-05 23:21:11 +0200

    support for bits to bytes conversion from trs to daredevil

--- trs2ddl.jl
+++ trs2ddl.jl
@@ -1,6 +1,6 @@
 # This file is part of Jlsca, license is GPLv3, see https://www.gnu.org/licenses/gpl-3.0.en.html
 #
-# Author: Cees-Bart Breunesse
+# Author: Cees-Bart Breunesse, Ilya Kizhvatov
 
 include("trs.jl")
 
@@ -11,13 +11,23 @@ using Trs
 # filename      - Inspector trs file name
 # dataStartByte - first data byte to take (starting from 1)
 # dataNumBytes  - number of data bytes to take
-function ins2daredevil(filename, dataOffset, dataBytes)
+# bitsToBytes   - represent individual bits from trs as bytes in split binary
+function trs2ddl(filename, dataOffset, dataBytes, bitsToBytes::Bool = true)
 
     trs = InspectorTrace(filename)
+
     (data,samples) = trs[1]
     numOriginalDataBytes = length(data)
     numOriginalSamples = length(samples)
     numOriginalTraces = length(trs)
+    sampleType = eltype(samples)
+
+    if bitsToBytes
+        # add samples pass that converts samples to bit array
+        addSamplePass(trs, tobits)
+        numOriginalSamples = numOriginalSamples * 8
+        sampleType = UInt8
+    end
 
     # assert a careless user
     if (dataOffset + dataBytes - 1 > numOriginalDataBytes)
@@ -26,12 +36,19 @@ function ins2daredevil(filename, dataOffset, dataBytes)
     end
 
     guessesfn = @sprintf("data_UInt8_%dt.bin", numOriginalTraces)
-    samplesfn = @sprintf("samples_%s_%dt.bin", eltype(samples), numOriginalTraces)
+    samplesfn = @sprintf("samples_%s_%dt.bin", sampleType, numOriginalTraces)
 
-    bin = SplitBinary(guessesfn, dataBytes, samplesfn, numOriginalSamples, eltype(samples), numOriginalTraces, true)
+    bin = SplitBinary(guessesfn, dataBytes, samplesfn, numOriginalSamples, sampleType, numOriginalTraces, true)
 
     for i in 1:length(trs)
-        bin[i] = (trs[i][1][dataOffset:dataOffset+dataBytes-1], trs[i][2])
+        s = trs[i][2]
+
+        if bitsToBytes
+            # convert bit array to bytes
+            s = UInt8.(s)
+        end
+
+        bin[i] = (trs[i][1][dataOffset:dataOffset+dataBytes-1], s)
     end
 
 
@@ -43,4 +60,4 @@ function ins2daredevil(filename, dataOffset, dataBytes)
     close(bin)
 end
 
-ins2daredevil(ARGS[1], 1, 16)
+trs2ddl(ARGS[1], 1, 16)
