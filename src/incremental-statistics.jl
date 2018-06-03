f6687b10dc3d4509f8c4b70f3e052195914c3775
Cees-Bart Breunesse <ceeesb@gmail.com>
2018-06-03 15:41:08 -0700

    * fix for Julia 0.6.3: I hacked in the threading behavior from 0.6.2 since the 0.6.3 threading is killing my performance for a reason I do not understand.

--- src/incremental-statistics.jl
+++ src/incremental-statistics.jl
@@ -278,16 +278,101 @@ function dothreadwork(state::IncrementalCovarianceTiled, y::Int)
   end
 end
 
+# Only read/written by the main thread
+const in_threaded_loop = Ref(false)
+
+import Base.Threads.threadid
+import Base.Threads.nthreads
+
+function _xthreadsfor(iter,lbody)
+    lidx = iter.args[1]         # index
+    range = iter.args[2]
+    quote
+        local threadsfor_fun
+        range = $(esc(range))
+        function threadsfor_fun(onethread=false)
+            r = range # Load into local variable
+            lenr = length(r)
+            # divide loop iterations among threads
+            if onethread
+                tid = 1
+                len, rem = lenr, 0
+            else
+                tid = threadid()
+                len, rem = divrem(lenr, nthreads())
+            end
+            # not enough iterations for all the threads?
+            if len == 0
+                if tid > rem
+                    return
+                end
+                len, rem = 1, 0
+            end
+            # compute this thread's iterations
+            f = 1 + ((tid-1) * len)
+            l = f + len - 1
+            # distribute remaining iterations evenly
+            if rem > 0
+                if tid <= rem
+                    f = f + (tid-1)
+                    l = l + tid
+                else
+                    f = f + rem
+                    l = l + rem
+                end
+            end
+            # run this thread's iterations
+            for i = f:l
+                local $(esc(lidx)) = Base.unsafe_getindex(r,i)
+                $(esc(lbody))
+            end
+        end
+        # code_warntype(STDOUT, threadsfor_fun, (Bool,))
+        # end
+        # Hack to make nested threaded loops kinda work
+        if threadid() != 1 || in_threaded_loop[]
+            # We are in a nested threaded loop
+            threadsfor_fun(true)
+        else
+            in_threaded_loop[] = true
+            # the ccall is not expected to throw
+            ccall(:jl_threading_run, Ref{Void}, (Any,), threadsfor_fun)
+            in_threaded_loop[] = false
+        end
+        nothing
+    end
+end
+
+
+macro xthreads(args...)
+    na = length(args)
+    if na != 1
+        throw(ArgumentError("wrong number of arguments in @threads"))
+    end
+    ex = args[1]
+    if !isa(ex, Expr)
+        throw(ArgumentError("need an expression argument to @threads"))
+    end
+    if ex.head === :for
+        return _xthreadsfor(ex.args[1],ex.args[2])
+    else
+        throw(ArgumentError("unrecognized argument to @threads"))
+    end
+end
+
+
 function flushcache!(state::IncrementalCovarianceTiled)
   if state.cacheCount == 0
     return
   end
 
-  Threads.@threads for y in 1:state.nrTilesY
+  # Threads.@threads for y in 1:state.nrTilesY
+  @xthreads for y in 1:state.nrTilesY
     dothreadwork(state,y)
   end
 
   state.cacheCount = 0
+  nothing
 end
 
 function storecache(cache::Vector{Float64}, data, datamean)
